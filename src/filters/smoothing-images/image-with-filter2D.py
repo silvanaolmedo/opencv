6485f777050e9d73acf155df5e72669314819532
silvanaolmedo <silvana.olmedo@comunidad.ub.edu.ar>
2017-06-11 18:54:50 -0300

    some fixies

--- src/filters/smoothing-images/image-with-filter2D.py
+++ src/filters/smoothing-images/image-with-filter2D.py
@@ -9,7 +9,6 @@ image_resized = cv2.resize(image,(width/2,height/2))
 cv2.imshow("Original",image_resized)
 
 filter2d = cv2.filter2D(image_resized,-1,(5,5))
-
 cv2.imshow("Image with filter2D",filter2d)
 
 cv2.waitKey(0)
