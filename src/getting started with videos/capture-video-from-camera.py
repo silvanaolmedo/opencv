5999c23d27391dc88ef6ce9e606087e5aff982af
silvanaolmedo <silvana.olmedo@comunidad.ub.edu.ar>
2017-06-12 20:37:54 -0300

    capturing video from webcam

---
+++ src/getting started with videos/capture-video-from-camera.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+
+while(True):
+	ret, frame = cap.read()
+
+	# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+	# cv2.imshow("Frame in gray", gray)
+	cv2.imshow("Frame", frame)
+	if cv2.waitKey(1) == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
+
